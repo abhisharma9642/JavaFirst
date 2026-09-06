5e659b55f219fc64ee3ea8ad05b808e1597717c0
abhisharma9642 <abhisharma9642@gmail.com>
2026-09-06 19:11:34 +0530

    Modified Java File

--- src/com/learn/Code42Constructor.java
+++ src/com/learn/Code42Constructor.java
@@ -1,37 +1,17 @@
-package com.learn;
+package com.learn
 
-class classOne
-{
-    private int number1;
-    private String naam1;
-    public classOne(int number2, String naam2)
-    {
-        this.naam1= naam2;
-        this.number1= number2;
+internal class classOne
+    (val number1: Int, val naam1: String?)
+
+
+object Code42Constructor {
+    @kotlin.jvm.JvmStatic
+    fun main(args: Array<String>) {
+        println("Java Constructor in Programming Logic")
+        val o = classOne(2, "sharma")
+        println(o.number1)
+        println(o.naam1)
     }
-    public int getNumber1()
-    {
-        return number1;
-    }
-    public String getNaam1()
-    {
-        return naam1;
-    }
-}
-
-
-
-public class JavaConstructor
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Java Constructor in Programming Logic");
-        classOne o= new classOne(2, "sharma");
-        System.out.println(o.getNumber1());
-        System.out.println(o.getNaam1());
-
-    }
-}
-//constructor will be always be in class itself, not outside the class defined already before
+} //constructor will be always be in class itself, not outside the class defined already before
 //constructors in Java programming can be overloaded with different parameters or arguments
 //with same name of constructors
